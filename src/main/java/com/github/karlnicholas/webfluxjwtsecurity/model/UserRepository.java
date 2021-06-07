b61661c92043e38c2e9d941e2c01541aa5cb6a93
Karl Nicholas <Karl.Nicholas@outlook.com>
2021-06-07 00:45:57 -0400

    Excess

--- src/main/java/com/github/karlnicholas/webfluxjwtsecurity/model/UserRepository.java
+++ src/main/java/com/github/karlnicholas/webfluxjwtsecurity/model/UserRepository.java
@@ -1,6 +1,5 @@
 package com.github.karlnicholas.webfluxjwtsecurity.model;
 
-import org.reactivestreams.Publisher;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import org.springframework.stereotype.Repository;
 
